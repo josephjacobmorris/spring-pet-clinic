2427e28d142948dd014e1ca7c2529d9ff467791e
Joseph Jacob <josephjacob1998@gmail.com>
2023-01-22 19:35:04 +0530

    adding context config for tc

--- pet-clinic-data/src/test/java/com/example/SpringPetClinicApplicationTests.java
+++ pet-clinic-data/src/test/java/com/example/SpringPetClinicApplicationTests.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = App.class)
 class SpringPetClinicApplicationTests {
 
 	@Test

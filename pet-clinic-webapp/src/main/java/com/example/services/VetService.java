304448870ac81e8ead58206877c5cf2abcbc2925
Joseph Jacob <josephjacob1998@gmail.com>
2023-01-22 20:00:45 +0530

    implement interface for services

---
+++ pet-clinic-webapp/src/main/java/com/example/services/VetService.java
@@ -0,0 +1,15 @@
+package com.example.services;
+
+import com.example.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+    Vet findByLastName(String lastName);
+}

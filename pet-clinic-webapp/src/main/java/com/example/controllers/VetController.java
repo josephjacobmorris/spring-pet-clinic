d7abd72a0878f5cac87d418ebfc0d0baa4684921
Joseph Jacob <josephjacob1998@gmail.com>
2023-01-27 08:58:32 +0530

    list vets impl

--- pet-clinic-webapp/src/main/java/com/example/controllers/VetController.java
+++ pet-clinic-webapp/src/main/java/com/example/controllers/VetController.java
@@ -1,12 +1,23 @@
 package com.example.controllers;
 
+import com.example.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String ownersIndex() {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
+    @RequestMapping({"/vets","/vets/", "/vets/index", "/vets/index.html"})
+    public String ownersIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }

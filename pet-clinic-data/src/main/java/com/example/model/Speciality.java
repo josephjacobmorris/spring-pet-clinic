e76af4b4e4e67ccb2ea1942f698f8bab80258b79
Joseph Jacob <josephjacob1998@gmail.com>
2023-02-05 12:06:55 +0530

    convert vets and specialties to JPA entities and apply relationship between them

--- pet-clinic-data/src/main/java/com/example/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/model/Speciality.java
@@ -1,5 +1,10 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
     private String description;
 
